4d011fe51325f1e73c8f4a15069d2db131b6c599
Dan Katz <dgkatz@gmail.com>
2020-09-10 12:16:53 -0400

    run migrations

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+import flask_migrate
 
 app = Flask(__name__)
 
@@ -16,6 +17,15 @@ app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+migrate = flask_migrate.Migrate(
+    db=db, directory="../migrations", render_as_batch=False
+)
+
+migrate.init_app(app)
+
+with app.app_context():
+    migrate.upgrade()
 
 from project.server.auth.views import auth_blueprint
 app.register_blueprint(auth_blueprint)
